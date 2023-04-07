154b032ce989f541c81ed82e3f785c1f4bd10749
Toadytop <tom.philip.ca@gmail.com>
2023-04-07 18:50:29 -0400

    Tasks

--- src/override/Home.vue
+++ src/override/Home.vue
@@ -1,13 +1,35 @@
-<script setup lang="ts">
-// import { storageDemo } from '~/logic/storage'
+<script setup>
+import { taskList } from '~/logic/storage'
 
 // function openOptionsPage() {
 //   browser.runtime.openOptionsPage()
 // }
+function addTask(task) {
+  taskList.value.push(task.target.value)
+  task.target.value = ''
+}
+function removeTask(task) {
+  taskList.value.splice(task, 1)
+}
+function clearTasks() {
+  taskList.value = []
+}
 </script>
 
 <template>
   <div class="w-full h">
-    test
+    <div v-for="(task, key) in taskList" :key="key">
+      <div>
+        <input type="button" value="x" @click="removeTask(key)"> {{ task }}
+        <br>
+      </div>
+    </div>
   </div>
-</template> 
+  <input class="border" @keyup.enter="addTask">
+  <br>
+  <input class="border" @keyup.enter="removeTask">
+  <br>
+  <button class="border" @click="clearTasks">
+    clearTasks
+  </button>
+</template>
