06d365d122ad2c9f3f2845aa4c4376b145e0c601
dmrgn <danielrmorgan11@gmail.com>
2023-04-08 15:37:01 -0400

    layout

--- src/components/FavoritedMenu.vue
+++ src/components/FavoritedMenu.vue
@@ -1,19 +1,27 @@
 <template>
+    <h2 class="mx-3 text-2xl font-bold">Favourites</h2>
     <div class="grid grid-cols-3">
-        <a class="text-center p-4 m-3 relative aspect-square bg-white rounded" v-for="(item, key) in favorites" :key="key" :href="item.url">
-            <div>
-                <img class="rounded aspect-square" alt="error" :src="item.url" @error="imageError">
-                <p class="absolute bottom-0">{{item.name}}</p>
-            </div>
-        </a>
+        <div class="text-center p-4 m-3 mb-2 relative aspect-square bg-white rounded-lg border-solid border-4 border-gray-300" v-for="(item, key) in favorites" :key="key">
+            <a class="" :href="item.url">
+                <img class="rounded-lg aspect-square" alt="error" :src="item.url" @error="imageError">
+                <p class="relative bottom-0 text-center mb-[-16px]">{{item.name}}</p>
+            </a>
+            <button :class="'absolute top-[-10px] right-[-10px] p-4 bg-white rounded-lg border-solid border-4 border-gray-300 aspect-square shadow to-show' + (isEditing ? 'shown': '')" @click="removeFavorite(key)">
+                <div class="absolute top-5px bg-black w-[2px] h-[20px]" style="transform:rotate(-45deg)"></div>
+                <div class="absolute top-5px bg-black w-[2px] h-[20px]" style="transform:rotate(45deg)"></div>
+            </button>
+        </div>
+        <div class="m-3 mb-2" v-if="favorites.length === 0">
+            <p>Looks like you haven't favourited anything yet...</p>
+        </div>
     </div>
     <div class="flex flex-col m-3">
-        <button class="my-2 rounded bg-white w-20 p-2" @click="toggleEditing">Edit</button>
-        <input placeholder=" Name" :class="'my-2 p-2 rounded to-show ' + (isEditing ? 'shown': '')" style="width: max(20%, 250px)" ref="favoriteName">
-        <input placeholder=" Url" :class="'my-2 p-2 rounded to-show ' + (isEditing ? 'shown': '')" style="width: max(20%, 250px)" ref="favoriteUrl">
-        <input placeholder=" Image Url" :class="'my-2 p-2 rounded to-show ' + (isEditing ? 'shown': '')" style="width: max(20%, 250px)" ref="favoriteImage">
-        <p class="my-2 ml-1 text-red-600 font-bold to-show" v-if="errorMessage">{{errorMessage}}</p>
-        <button class="my-2 rounded to-show bg-white w-20 p-2" @click="addFavorite()">Add</button>
+        <button class="mb-2 rounded-lg border-solid border-4 border-gray-300 bg-white w-20 p-2" @click="toggleEditing">Edit</button>
+        <input placeholder=" Name" :class="'my-2 p-2 rounded-lg border-solid border-4 border-gray-300 to-show ' + (isEditing ? 'shown': '')" style="width: max(20%, 250px)" ref="favoriteName">
+        <input placeholder=" Url" :class="'my-2 p-2 rounded-lg border-solid border-4 border-gray-300 to-show ' + (isEditing ? 'shown': '')" style="width: max(20%, 250px)" ref="favoriteUrl">
+        <input placeholder=" Image Url" :class="'my-2 p-2 rounded-lg border-solid border-4 border-gray-300 to-show ' + (isEditing ? 'shown': '')" style="width: max(20%, 250px)" ref="favoriteImage">
+        <p :class="'my-2 ml-1 font-bold text-red-600 to-show' + (isEditing ? 'shown': '')" v-if="errorMessage">{{errorMessage}}</p>
+        <button :class="'my-2 rounded-lg border-solid border-4 border-gray-300 to-show bg-white w-20 p-2 ' + (isEditing ? 'shown': '')" @click="addFavorite()">Add</button>
     </div>
     <br>
 </template>
@@ -24,7 +32,7 @@ import { favoritesStorage } from '../logic/storage';
 const ERROR_MESSAGES = {
     url: ["Thats not a real URL...", "Ur URL is invalid.", "Come back with a better URL!"],
     image: ["What kind of image is that...", "Bro... fix your image.", "Thats not a valid image URL!"],
-    tooMany: ["You can't have that many favourites!", "Too many favourites!", "Thats not a valid image URL!"],
+    tooMany: ["You can't have that many favourites!", "Too many favourites!", "How can you have that many favourites!"],
 };
 
 function isValidURL(str: string) {
@@ -74,6 +82,9 @@ export default {
             e.target.src = "https://imgs.search.brave.com/2ZzQSooIyRSbIhX7hO4xd55xxVeZVnBGmOJ4ys62bp8/rs:fit:432:225:1/g:ce/aHR0cHM6Ly90c2Uy/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5W/RWhsU2owMV9zTnhV/RUZwR1lJUE13SGFJ/SSZwaWQ9QXBp";
         },
         toggleEditing() {
+            this.$refs.favoriteName.value = '';
+            this.$refs.favoriteUrl.value = '';
+            this.$refs.favoriteImage.value = '';
             this.isEditing = !this.isEditing;
         }
     },
@@ -82,12 +93,12 @@ export default {
 
 <style lang="scss" scoped>
     
-    // .to-show {
-    //     transition: all 1s;
-    //     transform: ;
-    // }
+    .to-show {
+        transform: scale(0, 0);
+        transition: transform 0.5s;
+    }
 
-    // .shown {
-
-    // }
+    .shown {
+        transform: scale(1);
+    }
 </style>
--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,41 +1,47 @@
 <template>
-  <h2 class="text-2xl font-bold text-red-400">Task List</h2>
-  <div v-for="(task, key) in taskList" :key="key">
-    <div class="flex text-xl">
-      <button class="p-2 bg-white rounded-lg w-13 border-solid border-4 border-red-300" type="button" @click="removeTask(key)" >
+    <h2 class="text-2xl mx-3 mt-3 font-bold">Tasks</h2>
+    <div v-for="(task, key) in taskList" :key="key">
+        <div class="flex text-xl m-3">
+            <button class="p-2 bg-white rounded-lg w-13 border-solid border-4 border-gray-300" type="button"
+                @click="removeTask(key)">
 
-      </button>
-      <p class="p-2 bg-white rounded-lg text-red-400 font-bold w-80 border-4 border-gray-100">{{ task }} </p>
+            </button>
+            <p class="p-2 ml-2 bg-white rounded-lg font-bold w-[80%] border-4 border-gray-300">{{ task }} </p>
+        </div>
     </div>
-  </div>
-  <input class="p-2 w-94 text-xl bg-white rounded-lg text-red-400 border-solid border-4 border-gray-300 placeholder-red-200 font-bold" @keyup.enter="addTask" placeholder="Add Task">
+    <input
+        class="p-2 m-3 text-xl bg-white rounded-lg border-solid border-4 border-gray-300  font-bold"
+        style="width: max(80%, 20rem)"
+        @keyup.enter="addTask" placeholder="Add Task">
 </template>
 
 <script lang="ts">
- import {taskList} from "~/logic";
-export default {
- data() {
-   return {
-     taskList: taskList
-   }
- },
- methods: {
-   addTask(task: Element) {
-     console.log(task);
-     taskList.value.push(task.target.value)
-     task.target.value = ''
-   },
-   removeTask(task: number) {
-     taskList.value.splice(task, 1)
-   },
- }
- }
+    import {
+        taskList
+    } from "~/logic";
+    export default {
+        data() {
+            return {
+                taskList: taskList
+            }
+        },
+        methods: {
+            addTask(task: Element) {
+                console.log(task);
+                taskList.value.push(task.target.value)
+                task.target.value = ''
+            },
+            removeTask(task: number) {
+                taskList.value.splice(task, 1)
+            },
+        }
+    }
 </script>
 
 <style lang="scss" scoped>
-p {
-  text-overflow: ellipsis;
-  white-space: nowrap;
-  overflow: hidden;
-}
+    p {
+        text-overflow: ellipsis;
+        white-space: nowrap;
+        overflow: hidden;
+    }
 </style>
