fdc92503eff981025842af2afdb903f0f299c4d7
Toadytop <tom.philip.ca@gmail.com>
2023-04-09 21:03:22 -0400

    Minor changes to sky and clouds

--- src/components/Clouds.vue
+++ src/components/Clouds.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="absolute top-0 left-0 w-100vw h-100vh overflow-hidden">
-        <Wave :x="90" :size="60" :isFlippedX="false" :isFlippedY="false" :color="color"></Wave>
-        <Wave :x="50" :size="70" :isFlippedX="true" :isFlippedY="false" :color="color"></Wave>
+        <Wave :x="(60+40*Math.random())" :size="70" :isFlippedX="false" :isFlippedY="false" :color="color"></Wave>
+        <Wave :x="(60+40*Math.random())" :size="70" :isFlippedX="true" :isFlippedY="false" :color="color"></Wave>
     </div>
 </template>
 
